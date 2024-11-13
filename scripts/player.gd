7ed00b55b6d180f6e0b391558ea30f8b57cc7441
Ítalo H. <italodssouza.backup@gmail.com>
2024-11-13 11:07:11 -0300

    State machine

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,8 @@ const JUMP_VELOCITY = -350.0
 var player_life := 3
 var knockback_vector := Vector2.ZERO
 var is_jumping := false
+var is_hurted := false
+var direction
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -27,22 +29,19 @@ func _physics_process(delta: float) -> void:
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
 	
 	if knockback_vector != Vector2.ZERO:
 		velocity = knockback_vector
-
+	
+	_set_state()
+	
 	move_and_slide()
 
 
@@ -54,13 +53,11 @@ func _on_hurtbox_body_entered(body: Node2D) -> void:
 			take_damage(Vector2(-200,-200))
 		elif raycast_left.is_colliding():
 			take_damage(Vector2(200,-200))
-			
-		
-		
+
 func follow_camera(camera):
 	var camera_path = camera.get_path()
 	remote_transform.remote_path = camera_path
-	
+
 func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 	player_life -= 1
 	
@@ -71,3 +68,21 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 		knockback_tween.tween_property(self, "knockback_vector", Vector2.ZERO, duration)
 		animation.modulate = Color("red")
 		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration)
+		
+		is_hurted = true
+		await get_tree().create_timer(.3).timeout
+		is_hurted = false
+		
+func _set_state():
+	var state = "idle"
+
+	if !is_on_floor():
+		state = "jump"
+	elif direction != 0:
+		state = "run"
+
+	if is_hurted:
+		state = "hurt"
+
+	if animation.name != state:
+		animation.play(state)
