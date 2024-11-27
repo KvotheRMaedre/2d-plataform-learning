0aa5085492e6a3ae3ce1bfc6791975f802b00a91
Ítalo H. <italodssouza.backup@gmail.com>
2024-11-27 17:21:59 -0300

    BUGFIX: Initial position on death without getting a checkpoint

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -4,9 +4,11 @@ extends Node2D
 @onready var player_scene = preload("res://actors/player.tscn")
 @onready var camera := $camera as Camera2D
 @onready var control: Control = $HUD/control
+@onready var start_player_position: Marker2D = $start_player_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Globals.start_position = start_player_position
 	Globals.player = player
 	Globals.player.follow_camera(camera)
 	Globals.player.player_has_died.connect(reload_game)
@@ -18,6 +20,7 @@ func _process(_delta: float) -> void:
 
 func reload_game():
 	await get_tree().create_timer(1.0).timeout
+	control.reset_clock()
 	player = player_scene.instantiate()
 	add_child(player)
 	Globals.player = player
--- singletons/globals.gd
+++ singletons/globals.gd
@@ -5,14 +5,15 @@ var score := 0
 var player_life := 3
 
 var player = null
+var start_position = null
 var current_checkpoint = null
 
 func reset() -> void:
-	coins = 0
-	score = 0
 	player_life = 3
 
 func respawn_player() -> void:
-	Globals.reset()
+	reset()
 	if current_checkpoint != null:
 		player.position = current_checkpoint.global_position
+	else:
+		player.position = start_position.global_position
