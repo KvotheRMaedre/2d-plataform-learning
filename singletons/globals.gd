8e63f2a98b92d2d20558a8926cffed3246129d66
Ítalo H. <italodssouza.backup@gmail.com>
2024-11-28 13:19:15 -0300

    Changing how score and points reset on death or timeup

--- singletons/globals.gd
+++ singletons/globals.gd
@@ -9,10 +9,12 @@ var start_position = null
 var current_checkpoint = null
 
 func reset() -> void:
+	coins = 0
+	score = 0
 	player_life = 3
 
 func respawn_player() -> void:
-	reset()
+	player_life = 3
 	if current_checkpoint != null:
 		player.position = current_checkpoint.global_position
 	else:
