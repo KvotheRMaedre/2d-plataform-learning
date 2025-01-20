f9fa26f0ba8be819b7f2a2e5005418aaf167c619
Ítalo H. <italodssouza.backup@gmail.com>
2025-01-20 10:23:31 -0300

    Trying new moviments variables

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -3,4 +3,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		get_parent().anim.play("hurt")
-		body.velocity.y = body.JUMP_VELOCITY
+		body.velocity.y = -body.jump_velocity
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const AIR_FRICTION := 0.5
 
 @onready var animation := $anim as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
@@ -15,27 +15,42 @@ var is_jumping := false
 var is_hurted := false
 var direction
 
+@export var jump_height := 64
+@export var max_time_to_peak := 0.5
+
+var jump_velocity
+var gravity
+var fall_gravity
+
 signal player_has_died()
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _ready() -> void:
+	jump_velocity = (jump_height * 2) / max_time_to_peak
+	gravity = (jump_height * 2) / pow(max_time_to_peak, 2)
+	fall_gravity = gravity * 2
 
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.x = 0
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -jump_velocity
 		is_jumping = true
 		jump_sfx.play()
 	elif is_on_floor():
 		is_jumping = false
 		
+	if velocity.y > 0 or not Input.is_action_pressed("ui_accept"):
+		velocity.y += fall_gravity * delta
+	else:
+		velocity.y += gravity * delta
+		
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = lerp( velocity.x, direction * SPEED, AIR_FRICTION)
 		animation.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
